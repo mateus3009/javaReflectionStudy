3ced4c055d011ca86750810da17eb6f12812e29c
Mateus Sales Albino <mateus3009@gmail.com>
2021-10-30 08:56:38 -0300

    adicionando manipulador

--- src/main/ControleFaculdade.java
+++ src/main/ControleFaculdade.java
@@ -1,4 +1,11 @@
 package main;
+
+import java.lang.reflect.Field;
+
+import main.entidades.Aluno;
+import main.entidades.Pessoa;
+import main.servicos.Manipulador;
+
 /**
  * ControleFaculdade
  */
@@ -6,5 +13,11 @@ public class ControleFaculdade {
 
     public static void main(String[] args) {
         System.out.println(ControleFaculdade.class.getSimpleName());
+        var m = new Manipulador<>(Aluno.class);
+        m
+            .carregaCampos()
+            .stream()
+            .map(Field::getName)
+            .forEach(System.out::println);
     }
 }
--- src/main/servicos/Manipulador.java
+++ src/main/servicos/Manipulador.java
@@ -1,8 +1,15 @@
 package main.servicos;
 
 import java.lang.reflect.Field;
+import java.util.AbstractMap;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Manipulador <T> {
 
@@ -13,14 +20,55 @@ public class Manipulador <T> {
     }
 
     public List<Field> carregaCampos() {
-        throw new RuntimeException("Não implementado");
+        return carregaCampos(this.clazz)
+                .stream()
+                .map(c -> { c.setAccessible(true); return c; })
+                .collect(Collectors.toList());
     }
 
-    public T criaInstancia(Map<String, Object> values) {
-        throw new RuntimeException("Não implementado");
+    private List<Field> carregaCampos(Class clazz) {
+		var campos = Arrays.asList(clazz.getDeclaredFields());
+		if (clazz.getSuperclass() == null)
+			return campos;
+		return Stream
+			.of(carregaCampos(clazz.getSuperclass()), campos)
+			.flatMap(Collection::stream)
+			.collect(Collectors.toList());
+	}
+
+    public T criaInstancia(Map<String, Object> values) throws Exception {
+        var instancia = clazz.cast(clazz.getDeclaredConstructor().newInstance());
+        var campos =  carregaCampos();
+        for (var entry : values.entrySet()) {
+            campos
+                .stream()
+                .filter(c -> c.getName().equals(entry.getKey()))
+                .forEach(c -> defineValorDoCampo(c, instancia, entry.getValue()));
+        }
+        return instancia;
     }
 
-    public Map<String, Object> carregaValores(T object) {
-        throw new RuntimeException("Não implementado");
+    private void defineValorDoCampo(Field campo, T instancia, Object valor) {
+        try {
+            campo.set(instancia, valor);
+        } catch (Exception e) {
+            System.err.println("Não foi possível definir o valor do campo: " + campo.getName());
+        }
+    }
+
+    public Map<String, Object> carregaValores(T instancia) {
+        return carregaCampos()
+            .stream()
+            .map(c -> carregaValorDoCampo(c, instancia))
+            .collect(Collectors.toMap(Entry::getKey, Entry::getValue));
+    }
+
+    private Entry<String, Object> carregaValorDoCampo(Field campo, T instancia) {
+        try {
+            return new AbstractMap.SimpleEntry<String, Object>(campo.getName(), campo.get(instancia));
+        } catch (Exception e) {
+            System.err.println("Não foi possivel carregar o valor do campo: " + campo.getName());
+        }
+        return null;
     }
 }
